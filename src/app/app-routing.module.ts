be51d43aa5350dd40e59619f68dc9bd94020cd0d
windows <windows@microsoft.com>
2023-11-16 16:56:28 +0530

    change angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,33 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
-import { AboutusComponent } from './aboutus/aboutus.component';
-import { MyhomeComponent } from './myhome/myhome.component';
-import { MypipeComponent } from './mypipe/mypipe.component';
-import { LandingpageComponent } from './landingpage/landingpage.component';
-import { FeaturesComponent } from './features/features.component';
-
 const routes: Routes = [
-  {
-    path:'',
-    component:LandingpageComponent
-  },
-  {
-    path:'about',
-    component:AboutusComponent
-  },
-  {
-    path:'home',
-    component:MyhomeComponent
-  },
-  {
-    path:'pipe',
-    component:MypipeComponent
-  },
-  {
-    path:'mydirective',
-    component:FeaturesComponent
-  }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,29 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MyhomeComponent } from './myhome/myhome.component';
-import { AboutusComponent } from './aboutus/aboutus.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
 import { NgbAccordionModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
-import { MypipeComponent } from './mypipe/mypipe.component';
-import { LandingpageComponent } from './landingpage/landingpage.component';
-import { FeaturesComponent } from './features/features.component';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MyhomeComponent,
-    AboutusComponent,
-    HeaderComponent,
-    FooterComponent,
-    MypipeComponent,
-    LandingpageComponent,
-    FeaturesComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
