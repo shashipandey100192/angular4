166c56b979c21d977fd1dc8d174134abac8bb1ad
windows <windows@microsoft.com>
2023-11-22 16:59:39 +0530

    create login page

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { ForgetComponent } from './forget/forget.component';
 
 const routes: Routes = [
   {
     path:'',
     component:LoginComponent
+  },
+  {
+    path:'forget',
+    component:ForgetComponent
   }
 ];
 
