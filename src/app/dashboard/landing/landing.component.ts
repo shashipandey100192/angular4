b9b924510846e7c4f23a58b0ab1fdc477fb381a7
windows <windows@microsoft.com>
2023-11-27 16:51:39 +0530

    change

--- src/app/dashboard/landing/landing.component.ts
+++ src/app/dashboard/landing/landing.component.ts
@@ -7,4 +7,73 @@ import { Component } from '@angular/core';
 })
 export class LandingComponent {
 
+mycard:any[]=[
+  {
+    sno:1,
+    corse:"angular",
+    duration:"2 Months",
+    classtime:'4-5pm',
+    disc:'onsectetur animi nesciunt iure dolore\nenim quia ad\nveniam autem ut quam aut nobis\net est aut quod aut provident voluptas autem voluptas',
+    classimg:'https://wallpapers.com/images/featured/beautiful-3vau5vtfa3qn7k8v.jpg'
+  },
+  {
+    sno:2,
+    corse:"angular",
+    duration:"2 Months",
+    classtime:'4-5pm',
+    disc:'onsectetur animi nesciunt iure dolore\nenim quia ad\nveniam autem ut quam aut nobis\net est aut quod aut provident voluptas autem voluptas',
+    classimg:'https://wallpapers.com/images/featured/beautiful-3vau5vtfa3qn7k8v.jpg'
+  },
+  {
+    sno:3,
+    corse:"angular",
+    duration:"2 Months",
+    classtime:'4-5pm',
+    disc:'onsectetur animi nesciunt iure dolore\nenim quia ad\nveniam autem ut quam aut nobis\net est aut quod aut provident voluptas autem voluptas',
+    classimg:'https://wallpapers.com/images/featured/beautiful-3vau5vtfa3qn7k8v.jpg'
+  },
+  {
+    sno:4,
+    corse:"angular",
+    duration:"2 Months",
+    classtime:'4-5pm',
+    disc:'onsectetur animi nesciunt iure dolore\nenim quia ad\nveniam autem ut quam aut nobis\net est aut quod aut provident voluptas autem voluptas',
+    classimg:'https://wallpapers.com/images/featured/beautiful-3vau5vtfa3qn7k8v.jpg'
+  },
+  {
+    sno:5,
+    corse:"angular",
+    duration:"2 Months",
+    classtime:'4-5pm',
+    disc:'onsectetur animi nesciunt iure dolore\nenim quia ad\nveniam autem ut quam aut nobis\net est aut quod aut provident voluptas autem voluptas',
+    classimg:'https://wallpapers.com/images/featured/beautiful-3vau5vtfa3qn7k8v.jpg'
+  },
+  {
+    sno:6,
+    corse:"angular",
+    duration:"2 Months",
+    classtime:'4-5pm',
+    disc:'onsectetur animi nesciunt iure dolore\nenim quia ad\nveniam autem ut quam aut nobis\net est aut quod aut provident voluptas autem voluptas',
+    classimg:'https://wallpapers.com/images/featured/beautiful-3vau5vtfa3qn7k8v.jpg'
+  },
+  {
+    sno:7,
+    corse:"angular",
+    duration:"2 Months",
+    classtime:'4-5pm',
+    disc:'onsectetur animi nesciunt iure dolore\nenim quia ad\nveniam autem ut quam aut nobis\net est aut quod aut provident voluptas autem voluptas',
+    classimg:'https://wallpapers.com/images/featured/beautiful-3vau5vtfa3qn7k8v.jpg'
+  },
+  {
+    sno:8,
+    corse:"Angular.cli",
+    duration:"1 Months",
+    classtime:'4-5pm',
+    disc:'onsectetur animi nesciunt iure dolore\nenim quia ad\nveniam autem ut quam aut nobis\net est aut quod aut provident voluptas autem voluptas',
+    classimg:'https://wallpapers.com/images/featured/beautiful-3vau5vtfa3qn7k8v.jpg'
+  }
+];
+
+
+
 }
